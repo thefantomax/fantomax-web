f5e3726d8fe5085703afddf59428b1d13c5af037
riade2014 <feupes@yahoo.fr>
2024-04-10 12:00:44 +0200

    formulaire inscription/connexion

--- task-manager-web/src/app/app.module.ts
+++ task-manager-web/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { ConnexionComponent } from './pages/connexion/connexion.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    ConnexionComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
